581241bff92bf67dc0c5607405c164c4aaf72c57
teddyphotos <goelkashish4@gmail.com>
2020-02-27 19:16:42 +0530

    [CREATE] Created eventDetails.dart

---
+++ lib/eventDetails.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class eventDetail extends StatefulWidget {
+  @override
+  _eventDetailState createState() => _eventDetailState();
+}
+
+class _eventDetailState extends State<eventDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
